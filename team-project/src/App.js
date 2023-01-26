b828ecdb9bf3c081fdb337098cd8484ed0b30332
Anh Hoang <Anh.Hoang.2021@live.rhul.ac.uk>
2023-01-26 21:07:59 +0000

    Add pics to main webpage

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,4 +1,5 @@
 import logo from "./assets/Oxaca_Restaurants_Logo.png";
+import p1 from "./assets/p1.png";
 import "./App.css";
 
 function App() {
@@ -36,54 +37,10 @@ function App() {
           </li>
         </ul>
       </header>
-      <ul>
-        <li>
-          <input type="button" value="Menu" />
-        </li>
-        <li>
-          <input type="button" value="About" />
-        </li>
-        <li>
-          <input type="button" value="Contact" />
-        </li>
-        <li>
-          <input type="button" value="Waiter" />
-        </li>
-        <li>
-          <input type="button" value="Kitchen Staff" />
-        </li>
-      </ul>
-      <h2>Menu</h2>
-      <ul>
-        <li>
-          <h3>Taco al Pastor</h3>
-          <p>Marinated pork with pineapple</p>
-          <form>
-            <button type="submit">Order</button>
-          </form>
-        </li>
-        <li>
-          <h3>Carne Asada Taco</h3>
-          <p>Grilled steak with cilantro and onions</p>
-          <form>
-            <button type="submit">Order</button>
-          </form>
-        </li>
-        <li>
-          <h3>Chimichanga</h3>
-          <p>Fried burrito filled with meat and cheese</p>
-          <form>
-            <button type="submit">Order</button>
-          </form>
-        </li>
-        <li>
-          <h3>Beef Burrito</h3>
-          <p>Flour tortilla filled with beef, rice, beans, and salsa</p>
-          <form>
-            <button type="submit">Order</button>
-          </form>
-        </li>
-      </ul>
+
+      <div className="bg-indigo-300 my-10">
+        <img className="object-cover h-[600px] w-full" src={p1} />
+      </div>
     </div>
   );
 }
