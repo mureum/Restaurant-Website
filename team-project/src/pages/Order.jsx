ca4a50ff8d9445e4fadc0ed7b09984f98dd13011
Alelentini2001 <alelentini@live.com>
2023-02-05 01:47:26 +0000

    Created filter texfield for the items on the menu

--- team-project/src/pages/Order.jsx
+++ team-project/src/pages/Order.jsx
@@ -63,6 +63,17 @@ function Order() {
     }
   }
 
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+
+const handleChange = (e) => {
+  setSearchTerm(e.target.value);
+  let searchResults = items.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  setSearchResults(searchResults);
+};
+
   return (
     <div className="App">
 <Navbar />
@@ -112,26 +123,48 @@ function Order() {
           DR
         </button>
       </div>
+      <input type="text" value={searchTerm} onChange={handleChange} placeholder="Search for products.."/>
+
         <div className="items"  style={{  width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", margin: "0 auto", maxWidth: "900px"}}>
-            {items.map(item => (
-                <div className="item" 
-                  style={{
-                      display: activeBtn === "all" || activeBtn === item.type_id ? "block" : "none", 
-                      width: 'auto',
-                      flexBasis:"30%", 
-                      height: '100%', 
-                      backgroundColor: "red" 
-                    }} key={item.item_ID}>
-                    <img src={`https://www.themealdb.com/images/ingredients/${item.name}.png`} style={{width: "100%", height: "auto", objectFit: "cover", backgroundColor: "white"}} alt={`${item.name} image`} onError={e => e.target.src=`https://spoonacular.com/cdn/ingredients_100x100/${item.name}.jpg`}/>
-                    <h2>{item.name}</h2>
-                    <p>£{item.price}</p>
-                    <span>{item.calories}cal</span>
-                    <br></br>
-                    <button className="text-3xl font-bold text-yellow-100 uppercase space-x-3">
-                      <Link to="/">Add to cart</Link>
-                    </button>
-                </div>
-            ))}
+        {searchTerm
+    ? searchResults.map(item => (
+        <div className="item" 
+          style={{
+              display: activeBtn === "all" || activeBtn === item.type_id ? "block" : "none", 
+              width: 'auto',
+              flexBasis:"30%", 
+              height: '100%', 
+              backgroundColor: "red" 
+            }} key={item.item_ID}>
+            <img src={`https://www.themealdb.com/images/ingredients/${item.name}.png`} style={{width: "100%", height: "auto", objectFit: "cover", backgroundColor: "white"}} alt={`${item.name} image`} onError={e => e.target.src=`https://spoonacular.com/cdn/ingredients_100x100/${item.name}.jpg`}/>
+            <h2>{item.name}</h2>
+            <p>£{item.price}</p>
+            <span>{item.calories}cal</span>
+            <br></br>
+            <button className="text-3xl font-bold text-yellow-100 uppercase space-x-3">
+              <Link to="/">Add to cart</Link>
+            </button>
+        </div>
+      ))
+    : items.map(item => (
+        <div className="item" 
+          style={{
+              display: activeBtn === "all" || activeBtn === item.type_id ? "block" : "none", 
+              width: 'auto',
+              flexBasis:"30%", 
+              height: '100%', 
+              backgroundColor: "red" 
+            }} key={item.item_ID}>
+            <img src={`https://www.themealdb.com/images/ingredients/${item.name}.png`} style={{width: "100%", height: "auto", objectFit: "cover", backgroundColor: "white"}} alt={`${item.name} image`} onError={e => e.target.src=`https://spoonacular.com/cdn/ingredients_100x100/${item.name}.jpg`}/>
+            <h2>{item.name}</h2>
+            <p>£{item.price}</p>
+            <span>{item.calories}cal</span>
+            <br></br>
+            <button className="text-3xl font-bold text-yellow-100 uppercase space-x-3">
+              <Link to="/">Add to cart</Link>
+            </button>
+        </div>
+      ))}
         </div>
         <br></br>
     </div>
