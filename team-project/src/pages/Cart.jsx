7b4b3b5e313a7ef02a62bccf13706fdd5d68393e
Alelentini2001 <alelentini@live.com>
2023-02-08 23:35:08 +0000

    Created a Total variable showing the total price for the items

--- team-project/src/pages/Cart.jsx
+++ team-project/src/pages/Cart.jsx
@@ -25,6 +25,8 @@ const Cart = () => {
         }
       }, [items]); // here we add the dependency items to useEffect, so when the items are changed, the effect is triggered and sets the localStorage with the updated items.
 
+    var total = 0;
+
     return (
         <div className="App">
         <Navbar />
@@ -32,6 +34,7 @@ const Cart = () => {
             <div className="items"  style={{  width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", margin: "0 auto", maxWidth: "900px"}}>
             {cartItems && cartItems.length > 0 ? (
                 cartItems.map((item) => (
+                    <>
                     <div className={`item ${item.item_id%2===0 ? "rowA" : "rowB"}`}
                     style={{
                         width: 'auto',
@@ -42,9 +45,11 @@ const Cart = () => {
                         <img src={`https://www.themealdb.com/images/ingredients/${item.name}.png`} style={{width: "100%", height: "auto", objectFit: "cover", backgroundColor: "white"}} alt={`${item.name} image`} onError={e => e.target.src=`https://spoonacular.com/cdn/ingredients_100x100/${item.name}.jpg`}/>
                         <h2>{item.name}</h2>
                         <p>£{item.price}</p>
+                        {total = total + parseFloat(item.price)}
                         <br></br>
                         <button className="text-3xl font-bold text-yellow-100 uppercase space-x-3 delete" onClick={() => deleteFromCart(item.item_id)}>Delete</button>
                     </div>
+                    </>
                 ))
             ) : (
                 <div>
@@ -52,6 +57,9 @@ const Cart = () => {
                 </div>
             )}
           </div>  
+          <br></br>
+          <p> total = {total}</p>
+          <br></br><br></br>
         </div>
     );
 }
