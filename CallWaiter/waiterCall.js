8eecc73d5bc234de39049af2e3585a2c66857ed3
unknown <abulhasannabil@gmail.com>
2023-02-09 22:36:02 +0600

    connected to javascript

---
+++ CallWaiter/waiterCall.js
@@ -0,0 +1,19 @@
+var data ;
+function callWaiter() {
+    data = new FormData();
+    data.append("number", document.getElementById("table").value);
+    data.append("text", document.getElementById("problem").value);
+    if(data.get("number")==""){
+        window.alert("Please enter your table number!", );
+    }
+    else{
+        window.alert("Your request has been sent.");   
+        waiterCallInfo();
+    }   
+}
+
+function waiterCallInfo(){
+    var tableNumber = data.get("number");
+    var info = data.get("text");
+    document.querySelector('.tableNum').textContent ="5";
+}
