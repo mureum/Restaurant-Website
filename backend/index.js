8089cd515508f9d4cfea9ef2d6e03aef6f901835
Owen <zkac249@live.rhul.ac.uk>
2023-02-14 23:47:00 +0000

    Changed layout of login page. Fixed database querying, so logging in now fully works.

--- backend/index.js
+++ backend/index.js
@@ -5,30 +5,47 @@ import cors from 'cors'
 const app = express()
 app.use(express.json());
 
+
+
 const client = new pg.Client({
   connectionString: "postgresql://alelentini2001:v2_3yhPH_Kpa29MPQdzBAi6Ap8r8ug4d@db.bit.io:5432/alelentini2001/oaxaca",
   ssl: true
 })
 
-client.connect()
+client.connect((err) => {
+    if (err) {
+      console.error("Error connecting to PostgreSQL", err.stack);
+      process.exit(1);
+    }
+  });
+  
 app.use(cors())
+
+app.get("/", (req, res) => {
+    try {
+        res.json("Hello this is backend")
+    }catch (err) {
+        return res.json(err)
+    }
+})
+
+app.get("/logins", async (req,res)=>{
+    try {
+      const q = "SELECT * FROM logins;"
+      client.query(q, (err,data)=>{
+        if(err) throw err
+        return res.json(data.rows)
+      })
+    } catch (err) {
+      return res.json(err)
+    }
+  })
+  
+ 
+  
+  
+  
 
 app.listen(8800, ()=>{
     console.log("Connected to backend!")
-})
-
-
-app.post("/logins", (req,res)=>{
-    const q = "SELECT * FROM logins WHERE ? = username AND ? = password;"
-    client.query(q, (err,result)=>{
-        if (err) {
-            console.log(err);
-        }
-
-        if(result.length > 0){
-            res.send(result);
-        } else {
-            res.send({message : "Wrong login."})
-        }
-    })
 })
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import axios from 'axios';
-
-
+import React, { useEffect } from 'react';
+import Axios from 'axios';
+import "./App.css";
 
 function Login() {
-  const[username,setUsername] = React.useState('');
-  const[password,setPassword] = React.useState('');
-  const[text,setText] = React.useState('');
+  const[usernameInput,setUsernameInput] = React.useState('');
+  const[passwordInput,setPasswordInput] = React.useState('');
+  const[users,setUsers] = React.useState([]);
+  const[show,setShow] = React.useState(false);
+  
+  
+  useEffect(() => {
+    Axios.get("http://localhost:8800/logins").then((response) => {
+      setUsers(response.data);
+    })
+  })
 
   const Login = () => {
-    
-    axios.post("http://localhost:8800/logins", {
-      username : username,
-      password : password,
-    }).then((response) => {
-      setText(response);
-    });
+    for(var i = 0; i < users.length ; i++){
+      if(usernameInput === users[0].username && passwordInput === users[0].password){
+        console.log('TRUE');
+        setShow(false);
+      } else {
+        setShow(true);
+      }
+    }
+
   }
 
     return (
       <div>
         <h1>Login</h1>
-        <input type ="text" placeholder='Username' onChange = {(e) => {setUsername(e.target.value)}}/>
-        <input type ="text" placeholder='Password' onChange = {(e) => {setPassword(e.target.value)}}/>
-        <button style={{float: 'left'}} onClick = {() => Login()}>Login</button>
-        <h1>{text}</h1>
+        <div className ="form">
+          <label>Username:</label>
+          <input type ="text" placeholder='Username' onChange = {(e) => {setUsernameInput(e.target.value)}}/>
+          <label>Password:</label>
+          <input type ="text" placeholder='Password' onChange = {(e) => {setPasswordInput(e.target.value)}}/>
+        </div>
+        <button onClick = {Login}>Login</button>
+        <div style = {{color : 'red'}}> {show? <h1>INVALID LOGIN</h1> : null}</div>
       </div>
     );
   };
