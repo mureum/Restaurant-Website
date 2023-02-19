c66bdf033f404420725b89becf6df4a8f3a55aba
Haroon Zafar <zjac348@live.rhul.ac.uk>
2023-02-19 17:15:01 +0000

    second bunch of images
    **** bunch, havent tested, but it should be the same.

--- team-project/backend/database/images.sql
+++ team-project/backend/database/images.sql
@@ -18,3 +18,27 @@ VALUES ('110', 'https://images-gmi-pmc.edge-generalmills.com/42bd2679-29b7-4d02-
 
 INSERT INTO images (item_ID, link)
 VALUES ('111', 'https://growingupbilingual.com/wp-content/uploads/2015/09/IMG_7345.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1000', 'https://fishandmore.co.uk/wp-content/uploads/2021/08/fishmonger-near-me-breaded-cod-fillets-1.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1001', 'https://www.skiptomylou.org/wp-content/uploads/2018/04/chicken-enchilada-recipe.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1010', 'https://amandacooksandstyles.com/wp-content/uploads/2021/06/20210530_172208-500x500.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1011', 'https://brandsitesplatform-res.cloudinary.com/image/fetch/w_1540,c_scale,q_auto:eco,f_auto,fl_lossy,dpr_1.0,e_sharpen:85/https://assets.brandplatform.generalmills.com%2F-%2Fmedia%2Fproject%2Fgmi%2Foldelpaso%2Foldelpaso-uk%2Foepp%2Frecipes%2Froasted-vegetable-enchiladas%2Froasted-vegetable-enchiladas-hero.jpg%3Fw%3D800%26rev%3Dbf47ca0b54d44cf58a7cbfb531d44c28%201540w');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1100', 'https://www.mynourishedhome.com/wp-content/uploads/2019/11/burritos-800x800.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1101', 'https://images-gmi-pmc.edge-generalmills.com/3f2a43ee-fbec-46bc-b26a-0e6f64d85a41.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1110', 'https://assets.epicurious.com/photos/57978b27c9298e54495917d5/master/w_1000,h_662,c_limit/black-bean-and-vegetable-burritos.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('1111', 'https://takestwoeggs.com/wp-content/uploads/2021/12/Sriracha-mayo-chicken-quesadilla-with-chicken-and-rice-soup-Takestwoeggs-Knorr-Mexican-Sopa-SQ-500x375.jpg');
