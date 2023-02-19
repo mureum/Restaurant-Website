2eaf19b1fc0c0495763542580fc769d8396aadb8
Haroon Zafar <zjac348@live.rhul.ac.uk>
2023-02-19 17:01:09 +0000

    testing first 5
    first 5 images show on table!!

--- team-project/backend/database/images.sql
+++ team-project/backend/database/images.sql
@@ -1,5 +1,20 @@
 CREATE TABLE images (
     item_ID CHAR(6) NOT NULL,
     link VARCHAR(2555),
-    PRIMARY KEY (item_id)
+    PRIMARY KEY (item_ID)
 );
+
+INSERT INTO images (item_ID, link)
+VALUES ('11', 'https://cdn.loveandlemons.com/wp-content/uploads/2022/07/halloumi.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('100', 'https://simply-delicious-food.com/wp-content/uploads/2021/10/Chipotle-chicken-wings-1.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('101', 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FEdit%2F09-2022-chicken-potato-soup%2Fchicken-potato-soup-2');
+
+INSERT INTO images (item_ID, link)
+VALUES ('110', 'https://images-gmi-pmc.edge-generalmills.com/42bd2679-29b7-4d02-9813-d3cc2c9ee603.jpg');
+
+INSERT INTO images (item_ID, link)
+VALUES ('111', 'https://growingupbilingual.com/wp-content/uploads/2015/09/IMG_7345.jpg');
