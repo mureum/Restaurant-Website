c6bacc78cca0f3bb16b32c867dbed6a59d8d7399
Haroon Zafar <zjac348@live.rhul.ac.uk>
2023-02-22 17:51:09 +0000

    fixed image ids to match with other databases.
    ^^

--- team-project/backend/database/images.sql
+++ team-project/backend/database/images.sql
@@ -5,91 +5,91 @@ CREATE TABLE images (
 );
 
 INSERT INTO images (item_ID, link)
-VALUES ('11', 'https://cdn.loveandlemons.com/wp-content/uploads/2022/07/halloumi.jpg');
+VALUES ('000011', 'https://cdn.loveandlemons.com/wp-content/uploads/2022/07/halloumi.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('100', 'https://simply-delicious-food.com/wp-content/uploads/2021/10/Chipotle-chicken-wings-1.jpg');
+VALUES ('000100', 'https://simply-delicious-food.com/wp-content/uploads/2021/10/Chipotle-chicken-wings-1.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('101', 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FEdit%2F09-2022-chicken-potato-soup%2Fchicken-potato-soup-2');
+VALUES ('000101', 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FEdit%2F09-2022-chicken-potato-soup%2Fchicken-potato-soup-2');
 
 INSERT INTO images (item_ID, link)
-VALUES ('110', 'https://images-gmi-pmc.edge-generalmills.com/42bd2679-29b7-4d02-9813-d3cc2c9ee603.jpg');
+VALUES ('000110', 'https://images-gmi-pmc.edge-generalmills.com/42bd2679-29b7-4d02-9813-d3cc2c9ee603.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('111', 'https://growingupbilingual.com/wp-content/uploads/2015/09/IMG_7345.jpg');
+VALUES ('000111', 'https://growingupbilingual.com/wp-content/uploads/2015/09/IMG_7345.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1000', 'https://fishandmore.co.uk/wp-content/uploads/2021/08/fishmonger-near-me-breaded-cod-fillets-1.jpg');
+VALUES ('001000', 'https://fishandmore.co.uk/wp-content/uploads/2021/08/fishmonger-near-me-breaded-cod-fillets-1.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1001', 'https://www.skiptomylou.org/wp-content/uploads/2018/04/chicken-enchilada-recipe.jpg');
+VALUES ('001001', 'https://www.skiptomylou.org/wp-content/uploads/2018/04/chicken-enchilada-recipe.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1010', 'https://amandacooksandstyles.com/wp-content/uploads/2021/06/20210530_172208-500x500.jpg');
+VALUES ('001010', 'https://amandacooksandstyles.com/wp-content/uploads/2021/06/20210530_172208-500x500.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1011', 'https://brandsitesplatform-res.cloudinary.com/image/fetch/w_1540,c_scale,q_auto:eco,f_auto,fl_lossy,dpr_1.0,e_sharpen:85/https://assets.brandplatform.generalmills.com%2F-%2Fmedia%2Fproject%2Fgmi%2Foldelpaso%2Foldelpaso-uk%2Foepp%2Frecipes%2Froasted-vegetable-enchiladas%2Froasted-vegetable-enchiladas-hero.jpg%3Fw%3D800%26rev%3Dbf47ca0b54d44cf58a7cbfb531d44c28%201540w');
+VALUES ('001011', 'https://brandsitesplatform-res.cloudinary.com/image/fetch/w_1540,c_scale,q_auto:eco,f_auto,fl_lossy,dpr_1.0,e_sharpen:85/https://assets.brandplatform.generalmills.com%2F-%2Fmedia%2Fproject%2Fgmi%2Foldelpaso%2Foldelpaso-uk%2Foepp%2Frecipes%2Froasted-vegetable-enchiladas%2Froasted-vegetable-enchiladas-hero.jpg%3Fw%3D800%26rev%3Dbf47ca0b54d44cf58a7cbfb531d44c28%201540w');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1100', 'https://www.mynourishedhome.com/wp-content/uploads/2019/11/burritos-800x800.jpg');
+VALUES ('001100', 'https://www.mynourishedhome.com/wp-content/uploads/2019/11/burritos-800x800.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1101', 'https://images-gmi-pmc.edge-generalmills.com/3f2a43ee-fbec-46bc-b26a-0e6f64d85a41.jpg');
+VALUES ('001101', 'https://images-gmi-pmc.edge-generalmills.com/3f2a43ee-fbec-46bc-b26a-0e6f64d85a41.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1110', 'https://assets.epicurious.com/photos/57978b27c9298e54495917d5/master/w_1000,h_662,c_limit/black-bean-and-vegetable-burritos.jpg');
+VALUES ('001110', 'https://assets.epicurious.com/photos/57978b27c9298e54495917d5/master/w_1000,h_662,c_limit/black-bean-and-vegetable-burritos.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1111', 'https://takestwoeggs.com/wp-content/uploads/2021/12/Sriracha-mayo-chicken-quesadilla-with-chicken-and-rice-soup-Takestwoeggs-Knorr-Mexican-Sopa-SQ-500x375.jpg');
+VALUES ('001111', 'https://takestwoeggs.com/wp-content/uploads/2021/12/Sriracha-mayo-chicken-quesadilla-with-chicken-and-rice-soup-Takestwoeggs-Knorr-Mexican-Sopa-SQ-500x375.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10000', 'https://keviniscooking.com/wp-content/uploads/2019/05/Crispy-Ground-Beef-Quesadilla-square.jpg');
+VALUES ('010000', 'https://keviniscooking.com/wp-content/uploads/2019/05/Crispy-Ground-Beef-Quesadilla-square.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10001', 'https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/x17/20627-veggie-quesadillas-760x580.jpg?ext=.jpg');
+VALUES ('010001', 'https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/x17/20627-veggie-quesadillas-760x580.jpg?ext=.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10010', 'https://lmld.org/wp-content/uploads/2012/05/Chicken-Fajitas-1.jpg');
+VALUES ('010010', 'https://lmld.org/wp-content/uploads/2012/05/Chicken-Fajitas-1.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10011', 'https://keviniscooking.com/wp-content/uploads/2018/07/Grilled-Steak-Fajitas-square.jpg');
+VALUES ('010011', 'https://keviniscooking.com/wp-content/uploads/2018/07/Grilled-Steak-Fajitas-square.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10100', 'https://img.taste.com.au/y2pRo0Kn/taste/2016/11/prawn-fajitas-with-grilled-peach-salsa-89762-1.jpeg');
+VALUES ('010100', 'https://img.taste.com.au/y2pRo0Kn/taste/2016/11/prawn-fajitas-with-grilled-peach-salsa-89762-1.jpeg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10101', 'https://e4g6r5a9.rocketcdn.me/wp-content/uploads/2021/02/SambalFriedChickenBurger.jpg');
+VALUES ('010101', 'https://e4g6r5a9.rocketcdn.me/wp-content/uploads/2021/02/SambalFriedChickenBurger.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10110', 'https://www.oliveandmango.com/images/uploads/2021_06_21_classic_grilled_cheeseburger_1.jpg');
+VALUES ('010110', 'https://www.oliveandmango.com/images/uploads/2021_06_21_classic_grilled_cheeseburger_1.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10111', 'https://images.immediate.co.uk/production/volatile/sites/2/2016/06/23929.jpg?quality=90&resize=556,505');
+VALUES ('010111', 'https://images.immediate.co.uk/production/volatile/sites/2/2016/06/23929.jpg?quality=90&resize=556,505');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11000', 'https://veenaazmanov.com/wp-content/uploads/2020/04/Classic-Pizza-Margherita1.jpg');
+VALUES ('011000', 'https://veenaazmanov.com/wp-content/uploads/2020/04/Classic-Pizza-Margherita1.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11001', 'https://www.jennycancook.com/wp-content/uploads/2013/02/PeppPizza_600.jpg');
+VALUES ('011001', 'https://www.jennycancook.com/wp-content/uploads/2013/02/PeppPizza_600.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11010', 'https://hips.hearstapps.com/hmg-prod/images/210302-vegan-spiral-cover-cauliflower-pizza-086-eb-1626711437.jpg?crop=0.998xw:0.668xh;0.00160xw,0.240xh&resize=1200:*');
+VALUES ('011010', 'https://hips.hearstapps.com/hmg-prod/images/210302-vegan-spiral-cover-cauliflower-pizza-086-eb-1626711437.jpg?crop=0.998xw:0.668xh;0.00160xw,0.240xh&resize=1200:*');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11011', 'https://hips.hearstapps.com/hmg-prod/images/full-no-bake-cheesecake-horizontal-jpg-1525208292.jpg?crop=1.00xw:0.752xh;0,0.0697xh&resize=1200:*');
+VALUES ('011011', 'https://hips.hearstapps.com/hmg-prod/images/full-no-bake-cheesecake-horizontal-jpg-1525208292.jpg?crop=1.00xw:0.752xh;0,0.0697xh&resize=1200:*');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11100', 'https://www.biggerbolderbaking.com/wp-content/uploads/2018/05/Vanilla-2-Ingredient-Ice-cream-scaled.jpg');
+VALUES ('011100', 'https://www.biggerbolderbaking.com/wp-content/uploads/2018/05/Vanilla-2-Ingredient-Ice-cream-scaled.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11101', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Churros_Madrid.jpg/640px-Churros_Madrid.jpg');
+VALUES ('011101', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Churros_Madrid.jpg/640px-Churros_Madrid.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11110', 'https://nutritiouslife.com/wp-content/uploads/2010/05/shutterstock_274855409-copy-1302x988.jpg');
+VALUES ('011110', 'https://nutritiouslife.com/wp-content/uploads/2010/05/shutterstock_274855409-copy-1302x988.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('11111', 'https://simple-veganista.com/wp-content/uploads/2019/07/vegan-cobb-salad_.jpg');
+VALUES ('011111', 'https://simple-veganista.com/wp-content/uploads/2019/07/vegan-cobb-salad_.jpg');
 
 INSERT INTO images (item_ID, link)
 VALUES ('100000', 'https://media-cldnry.s-nbcnews.com/image/upload/t_social_share_1200x630_center,f_auto,q_auto:best/newscms/2021_28/1747160/coleslaw-main-khu-210712.jpg');
@@ -125,7 +125,7 @@ INSERT INTO images (item_ID, link)
 VALUES ('101010', 'https://bwhdrinks.co.uk/wp-content/uploads/2021/05/Belu-Sparkling-Water-330ml-1.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('10', 'https://minimalistbaker.com/wp-content/uploads/2014/04/The-Best-Damn-Vegan-Nachos.jpg');
+VALUES ('000010', 'https://minimalistbaker.com/wp-content/uploads/2014/04/The-Best-Damn-Vegan-Nachos.jpg');
 
 INSERT INTO images (item_ID, link)
-VALUES ('1', 'https://cookandlogic.com/wp-content/uploads/2022/08/receta-de-salsa-de-queso-para-tacos-y-nachos-1200x675.webp');
+VALUES ('000001', 'https://cookandlogic.com/wp-content/uploads/2022/08/receta-de-salsa-de-queso-para-tacos-y-nachos-1200x675.webp');
