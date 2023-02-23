e92b576a67a42bf4745d19a01fbadfdaa3b436fc
Anh Hoang <Anh.Hoang.2021@live.rhul.ac.uk>
2023-02-23 01:30:24 +0000

    Remove the search bar for calories

--- team-project/src/pages/Menu.jsx
+++ team-project/src/pages/Menu.jsx
@@ -406,39 +406,6 @@ function Order({ isLoggedIn, permission }) {
         </Link>
       </button>
       <br></br>
-      <div className="form-control">
-        <div className="input-group">
-          <input
-            type="text"
-            placeholder="Search for calories less than.."
-            title="Type in a calorie value"
-            className="input input-bordered w-96"
-            onChange={(e) => {
-              if (e.target.value !== "") {
-                setFilter(e.target.value);
-              } else {
-                setFilter("100000");
-              }
-            }}
-          />
-          <button className="btn btn-square">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
 
       <div className="grid-cols-1 gap-2 grid px-1 lg:grid-cols-2">
         {(items && permission === "Waiter") || permission === "Kitchen"
