e3e7663c823d7b26680b97d41918ca70109d5e02
MAHIR Tariq (2020) WJIS094 <tariq.mahir.2020@live.rhul.ac.uk>
2023-03-09 22:25:33 +0600

    connected with database

--- OrderDashboard/OrderDashboard.jsx
+++ OrderDashboard/OrderDashboard.jsx
@@ -6,78 +6,46 @@ import { OrderTable } from "./OrderTable";
 import { WaiterTable } from "./WaiterTable";
 import cooking from "../../assets/cooking.png";
 
-function OrderDashboard({ isLoggedIn, permission }) {
-  const [items, setItems] = useState([]);
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function RestaurantProgressBar() {
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    const fetchAlltems = async () => {
-      try {
-        const res = await axios.get("http://localhost:8800/pendingOrders");
-        const transformedData = res.data.map((item) => ({
-          tableNumber: item.table_no,
-          orderNumber: item.order_no,
-          customerName: item.customer_name,
-          time: item.time,
-          details: {},
-        }));
-        setItems(transformedData);
-      } catch (err) {
-        console.log(err);
+    axios.get('https://bit.io/ahmedshaed37/ORDER?view=tables#/orders', {
+      
+      headers: {
+        'Authorization': 'Bearer YOUR-BIT.IO-API-KEY'
       }
-    };
-
-    fetchAlltems();
-    console.log(items);
+    })
+    .then(response => {
+      setOrders(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }, []);
 
+  const getProgress = (status) => {
+    if (status === 'Not Started') {
+      return 0;
+    } else if (status === 'In Progress') {
+      return 50;
+    } else if (status === 'Ready') {
+      return 100;
+    }
+  }
+
   return (
-    <>
-      <div className="flex flex-col gap-10 container mx-auto">
-        <div className="form-control self-end">
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="Search…"
-              className="input input-bordered"
-            />
-            <button className="btn btn-square">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-          </div>
+    <div className="progress-bar">
+      {orders.map(order => (
+        <div key={order.id} className="progress-bar-item" style={{ width: `${getProgress(order.status)}%` }}>
+          <span className="progress-bar-text">{order.customerName}</span>
         </div>
-        <h1 className="text-3xl font-bold">
-          Pending orders <i className="fa-solid fa-clipboard"></i>
-        </h1>
-        <OrderTable nextStepText="Send to Kitchen" isCancellable={true} />
-        <h1 className="text-3xl font-bold flex gap-4 items-center">
-          <span>In preparation</span>
-          <img className="object-cover h-[40px] h-[40px]" src={cooking} />
-        </h1>
-        <OrderTable nextStepText="Mark as Ready" />
-        <h1 className="text-3xl font-bold">
-          Ready <i className="fa-solid fa-bell-concierge"></i>
-        </h1>
-        <OrderTable nextStepText="Mark as Delivered" />
-        <h1 className="text-3xl font-bold">
-          Delivered orders <i className="fa-solid fa-circle-check"></i>
-        </h1>
-        <OrderTable />
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
 
-export default OrderDashboard;
+export default RestaurantProgressBar;
