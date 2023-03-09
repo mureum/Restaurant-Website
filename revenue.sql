4ad65b734f24fd292cac0b84043f4b4436ff539f
Karen Bola <zjac126@cim-ts-node-03.cc.rhul.local>
2023-03-09 13:15:39 +0000

    Kareb Bola.Fixed a syntax error in the Stocklevel table. Added comments to my code for better reability.

--- revenue.sql
+++ revenue.sql
@@ -2,11 +2,13 @@
 
 CREATE TABLE daily_revenue (
     date DATE PRIMARY KEY,
-    total_revenue DECIMAL
+    total_revenue DECIMAL(7,2) 
     );
+--By the way, DECIMAL(7,2) Implies that over £9999.99 cannot be made in a day as it cannot store a higher value than that.
+
 
 -- SQL method to populate this table 
 INSERT INTO daily_revenue (date, total_revenue)
 SELECT order_date, SUM(total_price)
-FROM orders
+FROM allOrders
 GROUP BY order_date;
--- stocklevel.sql
+++ stocklevel.sql
@@ -1,6 +1,14 @@
+/* 
+This table measures the stock level
+'in_stock' shows the remaining quantity of each item
+'sold' shows the quantity of each item that has been sold
+'is_available' is TRUE when in_stock is greater than zero
+'is_available' is FALSE when in_stock equals zero
+*/
+
 CREATE TABLE stock_level(
     item_id NOT NULL,
-    is_available BOOL,
+    is_available BOOLEAN,
     in_stock int,
     sold int,
     PRIMARY KEY (item_id),
