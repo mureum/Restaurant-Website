ad06bbf193a9de1a28b9e812aeba45b4d36c5639
zkac227 <zkac227@live.rhul.ac.uk>
2023-03-15 13:39:58 +0000

    Statistics page is now able to connect with the database and issue has been solved

--- team-project/src/pages/statistics.jsx
+++ team-project/src/pages/statistics.jsx
@@ -3,15 +3,6 @@ import { useState, useEffect } from "react";
 import Axios from "axios";
 
 function FoodStatisticsTable() {
-  const foodItems = [
-    { name: 'Beef Burrito', cost: 200, revenue: 1000 },
-    { name: 'Beef Quesadilla', cost: 300, revenue: 1200 },
-    { name: 'Chicken Soup', cost: 100, revenue: 500 },
-    { name: 'Grilled Halloumi', cost: 150, revenue: 750 },
-    { name: 'Chipotle Wings', cost: 180, revenue: 900 },
-    { name: 'Nachos vegan', cost: 120, revenue: 600 },
-    // add more food items here...
-  ];
 
   const [items, setItems] = useState([]);
 
@@ -28,11 +19,7 @@ function FoodStatisticsTable() {
     fecthAllItems();
   }, []);
 
-  const totalCost = foodItems.reduce((sum, item) => sum + item.cost, 0);
-  const totalRevenue = foodItems.reduce((sum, item) => sum + item.revenue, 0);
-  const totalProfit = totalRevenue - totalCost;
-  const firstFoodItem = foodItems[0].name;
-  console.log(firstFoodItem);
+  
 
   return (
     <div>
@@ -41,20 +28,12 @@ function FoodStatisticsTable() {
         <thead>
           <tr>
             <th>Food Item</th>
-            <th>Cost</th>
+            <th>Price</th>
             <th>Revenue</th>
             <th>Profit</th>
           </tr>
         </thead>
         <tbody>
-          {foodItems.map(item => (
-            <tr key={item.name}>
-              <td>{item.name}</td>
-              <td>${item.cost}</td>
-              <td>${item.revenue}</td>
-              <td>${item.revenue - item.cost}</td>
-            </tr>
-          ))}
           {items.map(item => (
             <tr key={item.item_id}>
               <td>{item.name}</td>
@@ -64,16 +43,8 @@ function FoodStatisticsTable() {
             </tr>
           ))}
         </tbody>
-        <tfoot>
-          <tr>
-            <td>Total</td>
-            <td>${totalCost}</td>
-            <td>${totalRevenue}</td>
-            <td>${totalProfit}</td>
-          </tr>
-        </tfoot>
+      
       </table>
-      <p>{firstFoodItem}</p>
     </div>
   );
 
