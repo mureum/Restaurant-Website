94c53a3c00b4c952d52d4f6068d0fe77644023f5
Karen Bola <zjac126@cim-ts-node-03.cc.rhul.local>
2023-03-15 15:58:00 +0000

    Karen Bola. Added missing atrribute to the electronic payments table.

--- payment_info.sql
+++ payment_info.sql
@@ -1,6 +1,7 @@
 CREATE TABLE payment_info(
     transaction_id int NOT NULL,
     card_no varchar(16) NOT NULL,
+    cardholdder varchar (50) NOT NULL,
     exp_month smallint NOT NULL,
     exp_year smallint NOT NULL,
     PRIMARY KEY (transaction_id)
