088c38e87d6294a92d5465b9dbbbb51036196c8c
Karen Bola <zjac126@cim-ts-node-03.cc.rhul.local>
2023-03-15 15:30:55 +0000

    Karen Bola. Committing the electronic payments table in sql

---
+++ payment_info.sql
@@ -0,0 +1,7 @@
+CREATE TABLE payment_info(
+    transaction_id int NOT NULL,
+    card_no varchar(16) NOT NULL,
+    exp_month smallint NOT NULL,
+    exp_year smallint NOT NULL,
+    PRIMARY KEY (transaction_id)
+);
