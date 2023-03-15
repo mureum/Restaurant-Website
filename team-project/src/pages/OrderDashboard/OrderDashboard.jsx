f8c29e0a2bb80fc59b32aa232f684e048cce2cc2
Anh Hoang <Anh.Hoang.2021@live.rhul.ac.uk>
2023-03-15 01:36:36 +0000

    Fix the code

--- team-project/src/pages/OrderDashboard/OrderDashboard.jsx
+++ team-project/src/pages/OrderDashboard/OrderDashboard.jsx
@@ -4,15 +4,11 @@ import { useState } from "react";
 import axios from "axios";
 import { OrderTable } from "./OrderTable";
 import cooking from "../../assets/cooking.png";
-import {
-  completeOrder,
-  markAsDelivered,
-  markAsReady,
-  sendToKitchen,
-} from "./orderFunctions";
+import { completeOrder, markAsReady, sendToKitchen } from "./orderFunctions";
 
 function OrderDashboard({ isLoggedIn, permission }) {
   const [items, setItems] = useState([]);
+  const [currentTab, setCurrentTab] = useState("Assistance");
 
   useEffect(() => {
     const fetchAlltems = async () => {
@@ -63,36 +59,96 @@ function OrderDashboard({ isLoggedIn, permission }) {
             </button>
           </div>
         </div>
-        <h1 className="text-3xl font-bold">
-          Pending orders <i className="fa-solid fa-clipboard"></i>
-        </h1>
-        <OrderTable
-          nextStepText="Send to Kitchen"
-          isCancellable={true}
-          endPoint="pendingOrders"
-          nextCb={sendToKitchen}
-        />
-        <h1 className="text-3xl font-bold flex gap-4 items-center">
-          <span>In preparation</span>
-          <img className="object-cover h-[40px] h-[40px]" src={cooking} />
-        </h1>
-        <OrderTable
-          nextStepText="Mark as Ready"
-          endPoint="currentOrders"
-          nextCb={markAsReady}
-        />
-        <h1 className="text-3xl font-bold">
-          Ready <i className="fa-solid fa-bell-concierge"></i>
-        </h1>
-        <OrderTable
-          nextStepText="Mark as Delivered"
-          endPoint="readyOrders"
-          nextCb={completeOrder}
-        />
-        <h1 className="text-3xl font-bold">
-          Delivered orders <i className="fa-solid fa-circle-check"></i>
-        </h1>
-        <OrderTable />
+        <div className="tabs tabs-boxed bg-transparent">
+          <a
+            className={
+              "tab text-3xl font-bold" +
+              (currentTab === "Assistance" ? " tab-active" : "")
+            }
+            onClick={() => setCurrentTab("Assistance")}
+          >
+            Assistance
+          </a>
+          <a
+            className={
+              "tab text-3xl font-bold" +
+              (currentTab === "Pending Orders" ? " tab-active" : "")
+            }
+            onClick={() => setCurrentTab("Pending Orders")}
+          >
+            Pending Orders
+          </a>
+          <a
+            className={
+              "tab text-3xl font-bold" +
+              (currentTab === "In Preparation" ? " tab-active" : "")
+            }
+            onClick={() => setCurrentTab("In Preparation")}
+          >
+            In Preparation
+          </a>
+          <a
+            className={
+              "tab text-3xl font-bold" +
+              (currentTab === "Ready" ? " tab-active" : "")
+            }
+            onClick={() => setCurrentTab("Ready")}
+          >
+            Ready
+          </a>
+          <a
+            className={
+              "tab text-3xl font-bold" +
+              (currentTab === "Delivered" ? " tab-active" : "")
+            }
+            onClick={() => setCurrentTab("Delivered")}
+          >
+            Delivered
+          </a>
+        </div>
+        <div className={currentTab === "Assistance" ? "show" : "hidden"}>
+          <h1 className="text-3xl font-bold">
+            Assistance <i class="fa-solid fa-circle-question"></i>
+          </h1>
+        </div>
+        <div className={currentTab === "Pending Orders" ? "show" : "hidden"}>
+          <h1 className="text-3xl font-bold">
+            Pending Orders <i className="fa-solid fa-clipboard"></i>
+          </h1>
+          <OrderTable
+            nextStepText="Send to Kitchen"
+            isCancellable={true}
+            endPoint="pendingOrders"
+            nextCb={sendToKitchen}
+          />
+        </div>
+        <div className={currentTab === "In Preparation" ? "show" : "hidden"}>
+          <h1 className="text-3xl font-bold flex gap-4 items-center">
+            <span>In Preparation</span>
+            <img className="object-cover h-[40px] h-[40px]" src={cooking} />
+          </h1>
+          <OrderTable
+            nextStepText="Mark as Ready"
+            endPoint="currentOrders"
+            nextCb={markAsReady}
+          />
+        </div>
+        <div className={currentTab === "Ready" ? "show" : "hidden"}>
+          <h1 className="text-3xl font-bold">
+            Ready <i className="fa-solid fa-bell-concierge"></i>
+          </h1>
+          <OrderTable
+            nextStepText="Mark as Delivered"
+            endPoint="readyOrders"
+            nextCb={completeOrder}
+          />
+        </div>
+        <div className={currentTab === "Delivered" ? "show" : "hidden"}>
+          <h1 className="text-3xl font-bold">
+            Delivered <i className="fa-solid fa-circle-check"></i>
+          </h1>
+          <OrderTable nextStepText="" nextCb={""} endPoint="delivered" />
+        </div>
       </div>
     </>
   );
