1156009411100557a3e1184bedb0904fbfb105be
zkac227 <zkac227@live.rhul.ac.uk>
2023-03-16 22:53:58 +0000

    Changing styling of statistics page

--- team-project/src/pages/statistics.jsx
+++ team-project/src/pages/statistics.jsx
@@ -1,6 +1,50 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const pageStyle = {
+  backgroundColor: "#f2f2f2",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "50px",
+};
+
+const headerStyle = {
+  textAlign: "center",
+  fontWeight: "bold",
+  fontSize: "2.5rem",
+  marginBottom: "50px",
+  fontFamily: "Arial",
+};
+
+const tableContainer = {
+  display: "flex",
+  justifyContent: "center",
+  gap: "50px",
+};
+
+const tableStyle = {
+  borderCollapse: "collapse",
+  width: "50%",
+  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+  fontFamily: "Arial",
+};
+
+const rowStyle = (index) => ({
+  backgroundColor: index % 2 === 0 ? "#fff" : "#f2f2f2",
+});
+
+const cellStyle = {
+  padding: "10px",
+};
+
+const totalRevenueStyle = {
+  textAlign: "center",
+  marginTop: "50px",
+  fontSize: "1.8rem",
+  fontFamily: "Arial",
+};
+
 function FoodStatisticsTable() {
   const [items, setItems] = useState([]);
   const [dailyRev, setDailyRev] = useState([]);
@@ -25,13 +69,11 @@ function FoodStatisticsTable() {
         console.log(error);
       }
     }
-
     fetchData();
   }, []);
 
   return (
     <div style={pageStyle}>
-
       {totalRev.length > 0 ? (
         <p style={totalRevenueStyle}>
           Total Revenue so far: $ {totalRev[0].total_revenue_sum}
@@ -44,73 +86,44 @@ function FoodStatisticsTable() {
         <table style={tableStyle}>
           <thead>
             <tr>
-              <th>Item ID</th>
-              <th>Is Available</th>
-              <th>In Stock</th>
-              <th>Sold</th>
+              <th style={cellStyle}>Item ID</th>
+              <th style={cellStyle}>Is Available</th>
+              <th style={cellStyle}>In Stock</th>
+              <th style={cellStyle}>Sold</th>
             </tr>
           </thead>
           <tbody>
-            {items.map((item) => (
-              <tr key={item.item_id}>
-                <td>{item.item_id}</td>
-                <td>{item.is_available}</td>
-                <td>{item.in_stock}</td>
-                <td>{item.sold}</td>
+            {items.map((item, index) => (
+              <tr key={item.item_id} style={rowStyle(index)}>
+                <td style={cellStyle}>{item.item_id}</td>
+                <td style={cellStyle}>{item.is_available}</td>
+                <td style={cellStyle}>{item.in_stock}</td>
+                <td style={cellStyle}>{item.sold}</td>
               </tr>
             ))}
           </tbody>
         </table>
+
         <table style={tableStyle}>
           <thead>
             <tr>
-              <th>Date</th>
-              <th>Total Revenue</th>
+              <th style={cellStyle}>Date</th>
+              <th style={cellStyle}>Total Revenue</th>
             </tr>
           </thead>
           <tbody>
-            {dailyRev.map((rev) => (
-              <tr key={rev.date}>
-                <td>{rev.date}</td>
-                <td>{rev.total_revenue}</td>
+            {dailyRev.map((rev, index) => (
+              <tr key={rev.date} style={rowStyle(index)}>
+                <td style={cellStyle}>{rev.date}</td>
+                <td style={cellStyle}>{rev.total_revenue}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+      
     </div>
   );
 }
-
-const pageStyle = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "50px",
-};
-
-const headerStyle = {
-  textAlign: "center",
-  fontWeight: "bold",
-  fontSize: "2rem",
-  marginBottom: "50px",
-};
-
-const tableContainer = {
-  display: "flex",
-  justifyContent: "center",
-  gap: "50px",
-};
-
-const tableStyle = {
-  borderCollapse: "collapse",
-  width: "50%",
-};
-
-const totalRevenueStyle = {
-  textAlign: "center",
-  marginTop: "50px",
-  fontSize: "1.5rem",
-};
 
 export default FoodStatisticsTable;
